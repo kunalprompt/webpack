b75fd913bc1fcdc777bb8dd52cd34ce80640fcaf
kunalprompt <kunal.sharma@unbxd.com>
2017-07-24 15:16:09 +0530

    feat(SASS-Grid-Compiler) - transpiling sass to css

---
+++ sass-grid/webpack.config.js
@@ -0,0 +1,35 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "./dest/bundled.css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const config = {
+    name: 'css',
+    entry: './source/grid.scss',
+    output: {
+        filename: './dest/[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [
+                		{loader: "css-loader"},
+                		{loader: "sass-loader"}
+                	],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }]
+    },
+    plugins: [
+        new CleanWebpackPlugin(paths=["./dest/"]),
+        extractSass
+    ]
+};
+
+module.exports = config;
